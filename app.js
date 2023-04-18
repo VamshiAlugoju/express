b745e791cb4e088bbef8d41312055f5fee9db55d
vamshi <alugojuvamshi@gmail.com>
2023-04-18 13:31:27 +0530

    form-added

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("first middleware");
-    next();
+
+app.use(bodyParser.urlencoded());
+
+app.use( "/add-product" , (req,res,next)=>{
+    res.send('<form action="/product" method="post"><input type="text" name="name" id="Name"><input type="text" name="size" id="size"><input type="submit" name="btn" id="btn" value="click"></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log("second middleware");
-    next();
+app.use("/product" , (req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.use( "/" , (req,res,next)=>{
+     
+    res.send("hellof")
 })
 
 app.listen(4000)
